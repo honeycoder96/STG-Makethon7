e1eae6725e2c25e9fe02a9018b9ea42129bbe4dd
SHREEMOY <ishreemoymishra@gmail.com>
2019-12-13 23:25:32 +0530

    landingpage

--- frontend/src/Components/Pages/Homepage.js
+++ frontend/src/Components/Pages/Homepage.js
@@ -1,69 +1,77 @@
 import React, { Component, useState } from 'react';
-//material ui component
-
-import { Grid, Typography, Hidden } from "@material-ui/core";
-import Button from "@material-ui/core/Button";
 import '../style.css';
-import { Color } from '../color';
-
-import "../style.css";
-
-
-const style = {
-   overFlow: "hidden",
-   marginTop: "60px",
-   marginBottom: "-30px",
-   square: {
-      border: "4px solid #ff0000c9",
-      padding: "20px",
-      borderRadius: "15px",
-      position: "absolute",
-      marginTop: "160px",
-      left: "626px",
-      marginRight: "40px"
-   },
-   square1: {
-      border: "2px solid red",
-      padding: "10px",
-      position: "absolute",
-
-      left: "46px",
-      marginRight: "40px",
-      marginTop: "70px",
-   },
-   back: {
-      width: "100%",
-      height: "100%",
-      //Put new background Image here of Size 1550 * 720 px
+import Landing from '../../images/landing.jpg';
+import Card1 from '../../images/card1.jpg';
+import Card2 from '../../images/card2.png';
 
 
-      //Good Boy don't touch anything else
-      backgroundSize: "cover",
-
-      backgroundPositionX: "0%",
-      minHeight: "95vh",
-   },
-   button: {
-      color: "white",
-      backgroundColor: "#ff3b01f5",
-      padding: '10px 23px',
-      borderRadius: '8px',
-      fontSize: '14px',
-      fontWeight: '600'
-   },
-
-};
-
-/* FIXME: When the target will be decided, remove the rel attribute.
- * It has been added here for security reasons.
- * Reference: https://mathiasbynens.github.io/rel-noopener/
- */
+//material ui component
 
 const Homepage = () => {
 
    return (
-      <div>
-      </div >
+
+<div>
+
+  
+   <br>
+   </br>
+<div class="jumbotron">
+
+  <div class="container-fluid text-center px-0">
+    
+  <img src={Landing} alt="placeholder 1500" class="img-responsive" object-fit="cover"></img>
+    <h1> Campaign Monitoring</h1>     
+    <p>Welcome to the page</p>
+    <button type="button" class="btn btn-primary">Know More </button>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column">
+  <div class="card">
+  <img class="card-img-top" src={Card1} alt="Card image cap"></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+   </div>
+</div>
+
+<div class="column">
+  <div class="card">
+  <img class="card-img-top" src={Card2} alt="Card image cap"></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+   </div>
+</div>
+  
+<div class="column">
+  <div class="card">
+  <img class="card-img-top" src={Card1} alt="Card image cap"></img>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+
+   </div>
+</div>
+  
+  
+</div>
+<footer class="container-fluid text-center">
+  <p>Footer Text</p>
+</footer>
+</div>
+
+    
    );
 };
 
