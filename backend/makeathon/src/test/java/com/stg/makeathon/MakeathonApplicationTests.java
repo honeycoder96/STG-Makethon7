1f114b2fa7caa80e9f1716bbaa54f62a5a40eb5b
kartikDodeja <Reed@LAPTOP-VDB7LD28>
2019-12-13 16:34:55 +0530

    backend

---
+++ backend/makeathon/src/test/java/com/stg/makeathon/MakeathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stg.makeathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
